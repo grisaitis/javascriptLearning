59166c87b7885546f0e88d5863c0a501bdae5021
William C Grisaitis <wgrisaitis@gmail.com>
2014-09-16 04:18:55 +0000

    change discussion from prototypes to class-like functions

--- objectOrientedStuff/customClass.js
+++ objectOrientedStuff/customClass.js
@@ -1,14 +1,14 @@
-// let's create some prototype functions
+// let's create some class-like functions
 
 // this can be done in two ways
 // way 1:
-var prototypeWay1 = function() {};
+var classWay1 = function() {};
 // or way 2:
-function prototypeWay2() {};
+function classWay2() {};
 
 // and then we can create "instances"
-var instance1 = new prototypeWay1();
-var instance2 = new prototypeWay2();
+var instance1 = new classWay1();
+var instance2 = new classWay2();
 
 // these instances have type `Object`, which we can verify with...
 var typeOfInstance1 = Object.prototype.toString(instance1);
