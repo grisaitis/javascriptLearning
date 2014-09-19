2156e50cdcf23f43933b27f589b3e7fbd49ef0cb
William C Grisaitis <wgrisaitis@gmail.com>
2014-09-19 01:20:10 +0000

    added OO/practice.js

---
+++ OO/practice.js
@@ -0,0 +1,17 @@
+// here I am redoing some code elsewhere to jog my memory...
+
+var DEBUG = true;
+var person1;
+
+// create generating function for class
+var Person1 = function(name){
+    name1 = name;
+    this.name2 = name;
+}
+if (DEBUG){
+    person1 = Person1('test name1');
+    // console.log(person1.name1);
+    console.log(person1.name2);
+}
+
+// create methods for that class
