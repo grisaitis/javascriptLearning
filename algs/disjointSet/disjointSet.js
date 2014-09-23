935dff9376a0daa259785aaf72aa6bf0051f77c9
William C Grisaitis <wgrisaitis@gmail.com>
2014-09-23 06:33:22 +0000

    bug fixes & cosmetic

--- algs/disjointSet/disjointSet.js
+++ algs/disjointSet/disjointSet.js
@@ -2,20 +2,20 @@
 var DisjointSet = function(n) { 
 	/* create a set of n items */
 	this.n = n; // # of items given
-	this.items = new Array(n); // item[i] := parent of i
-	for (var i=0;i<n;i++) items[i]=i;
+	this.parentOf = new Array(n); // parentOf[i] := parent of i
+	for (var i=0;i<n;i++) this.parentOf[i]=i;
 }
 
 DisjointSet.prototype.union = function(item1, item2){
 	/* create union between item1 and item2 */
 	// find root of item1, and set it's parent to the root of item2
 	var rootOfItem1 = this.find(item1);
-	this.items[rootOfItem1] = this.find(item2);
+	this.parentOf[rootOfItem1] = this.find(item2);
 }
 
 DisjointSet.prototype.find = function(item){
 	/* find root of item */
-	if (item == this.items[item]) return item; // root found
+	if (item == this.parentOf[item]) return item; // root found
 	else return this.find(item);
 }
 
